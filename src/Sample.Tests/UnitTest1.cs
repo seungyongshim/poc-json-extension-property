1566f9e6a7fc55279b4057f2aafb3dd40fc67b8d
SeungYongShim <syshim@me.com>
2022-10-16 09:05:17 +0900

    .

--- src/Sample.Tests/UnitTest1.cs
+++ src/Sample.Tests/UnitTest1.cs
@@ -1,14 +1,35 @@
+using System.Collections.Generic;
 using System;
 using Xunit;
-using static Sample.Prelude;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 namespace Sample.Tests;
+
+public record Dto(string A, string B)
+{
+    [JsonExtensionData]
+    public IDictionary<string, object> Extensions { get; set; } = new Dictionary<string, object>(StringComparer.Ordinal);
+}
 
 public class PreludeSpec
 {
     [Fact]
-    public void AddSuccess()
+    public void JsonExtensionData()
     {
-        Assert.Equal(3, add(1)(2));
+        
+        var rawJson = """
+        {
+            "A" : "A",
+            "B" : "B",
+            "C" : "C"
+        }
+        """;
+
+        var dto = JsonSerializer.Deserialize<Dto>(rawJson);
+
+        Assert.Equal("A", dto.A);
+        Assert.Equal("B", dto.B);
+        Assert.Equal("C", dto.Extensions["C"].ToString());
     }
 }
